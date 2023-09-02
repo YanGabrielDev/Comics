7b2b90b26a92a368846cde1bb68a611cad10df29
Yan <yangabriel2012@gmail.com>
2023-09-02 12:13:02 -0300

    feat: criando o contexto do carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { ThemeProvider } from "styled-components"
 import { Router } from "./routes"
 import { GlobalStyles } from "./styles/global-styles"
 import { theme } from "./styles/theme"
+import { CartContextProvider } from "./contexts/Cart"
 
 export function App  () {
     return(
-        <ThemeProvider theme={theme}>
+      <CartContextProvider>
+          <ThemeProvider theme={theme}>
             <Router/>
             <GlobalStyles/>
         </ThemeProvider>
+      </CartContextProvider>
+      
         
     )
 }
