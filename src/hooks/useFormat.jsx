39b6910080aaac122fb28a516df6ebf68aef136c
Yan <yangabriel2012@gmail.com>
2023-09-02 19:54:38 -0300

    fix: formatando zipcode

--- src/hooks/useFormat.jsx
+++ src/hooks/useFormat.jsx
@@ -6,5 +6,16 @@ const formatCurrency = (value) =>
     currency: "BRL",
   });
 
-return { formatCurrency };
+ const formatZipCode = (event, formik, inputName) => {
+    let value = event.target.value;
+    value = value.replace(/\D/g, '');
+
+    if (value.length > 5) {
+      value = `${value.slice(0, 5)}-${value.slice(5, 8)}`;
+    }{
+    formik.setFieldValue(inputName, value)
+  }
+  };
+
+return { formatCurrency, formatZipCode };
 }
