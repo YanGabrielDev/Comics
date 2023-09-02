131d682bc27239106ec08c59d41c9d8d377704b4
Yan <yangabriel2012@gmail.com>
2023-09-02 07:36:26 -0300

    style: criando estilo padrao do projeto

---
+++ src/styles/theme.tsx
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    body: "#F6F6F6",
+    main: "#FFF",
+    mainSecondary: "#f3f2f2",
+    text: "#000",
+    primary: "#0055ff",
+    primarySoft: "#ebf1ff",
+    primaryHover: "#002163",
+  },
+  font: {
+    weight: {
+      normal: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 800,
+    },
+    size: {
+      normal: "1rem",
+      medium: "1.25rem",
+      large: "2rem",
+      huge: "3rem"
+    },
+    family:{
+      baloo: "Baloo 2",
+      poppins: "Poppins"
+    }
+  },
+};
