6e4a564337095901a7ac78e5876b8bbf0c8c7f78
Yan <yangabriel2012@gmail.com>
2023-09-02 12:15:32 -0300

    feat: criando o component details

---
+++ src/components/Details/styles.js
@@ -0,0 +1,87 @@
+import { css, styled } from "styled-components";
+
+export const DetailsContainer = styled.div`
+  ${() => css`
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+
+    img {
+      height: 20rem;
+      width: 13rem;
+      border-radius: 0.5rem;
+    }
+
+    img.thanos-image {
+      margin: 0 auto;
+      height: 8rem;
+
+      animation: wiggle 5s ease-in-out infinite;
+
+      @keyframes wiggle {
+        0%,
+        100% {
+          transform: translateY(-20px);
+        }
+        50% {
+          transform: translateY(20px);
+        }
+      }
+    }
+    @media (max-width: 600px) {
+      flex-direction: column;
+    }
+  `}
+`;
+
+export const DetailsContainerHeader = styled.div`
+  ${({ theme }) => css`
+    padding: 1.25rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    p {
+      font-size: ${theme.font.size.medium};
+      font-family: ${theme.font.family.baloo};
+      font-weight: ${theme.font.weight.bold};
+      margin-bottom: 0;
+    }
+  `}
+`;
+export const DetailsDescription = styled.span`
+  ${({ theme }) => css`
+    font-weight: ${theme.font.weight.normal};
+    font-size: ${theme.font.size.medium};
+    font-family: "Poppins";
+  `}
+`;
+
+export const DetailsDescriptionContainer = styled.div`
+  ${({ theme }) => css`
+    padding: 1.25rem;
+    width: 100%;
+    height: auto;
+    background-color: ${theme.colors.mainSecondary};
+    border-radius: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+  `}
+`;
+export const DetailsPriceContainer = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: flex-end;
+  `}
+`;
+
+export const DetailsComicTitle = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.font.size.medium};
+    font-family: ${theme.font.family.baloo};
+    font-weight: ${theme.font.weight.bold};
+  `}
+`;
