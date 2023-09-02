90444677f3a59a709331e6a731981132140a1201
Yan <yangabriel2012@gmail.com>
2023-09-02 19:49:15 -0300

    feat: trocando o nome da rota

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,25 +4,29 @@ import { useFormat } from "../../hooks/useFormat";
 import { useNavigate } from "react-router-dom";
 import { ShopButton } from "../ShopButton";
 import { useCart } from "../../hooks/useCart";
+import { ShoppingCartIcon } from "lucide-react";
+
 export const Card = ({ id, title, image, extension, price, comic }) => {
   const { formatCurrency } = useFormat();
   const navigate = useNavigate();
   const hqImage = `${image}.${extension}`;
   const hqPrice = formatCurrency(price);
-  const {addComicsToCart} = useCart()
-  
+  const { addComicsToCart } = useCart();
+
   return (
     <CardContainer>
       <CardImage
         src={hqImage}
         loading="lazy"
         alt={title}
-        onClick={() => navigate("/comic-details", { state: { id: id } })}
+        onClick={() => navigate("/Comic-details", { state: { id: id } })}
       />
       <ComicsTitle title={title}>{title}</ComicsTitle>
       <CardFooter>
         <strong>{hqPrice}</strong>
-        <ShopButton onClick={() => addComicsToCart(comic)}/>
+        <ShopButton onClick={() => addComicsToCart(comic)}>
+          <ShoppingCartIcon />
+        </ShopButton>
       </CardFooter>
     </CardContainer>
   );
@@ -34,5 +38,5 @@ Card.propTypes = {
   image: P.string.isRequired,
   extension: P.string.isRequired,
   price: P.number.isRequired,
-  comic: P.object.isRequired
+  comic: P.object.isRequired,
 };
