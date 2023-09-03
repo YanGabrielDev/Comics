0631eccb44b30a1e1ba1ab3868fb97f34befe732
Yan <yangabriel2012@gmail.com>
2023-09-03 17:52:33 -0300

    fix: corrigindo cors

--- src/services/restApiConstants.js
+++ src/services/restApiConstants.js
@@ -1,4 +1,4 @@
-export const BASE_URL_MARVEL_API = "http://gateway.marvel.com/v1/public"
+export const BASE_URL_MARVEL_API = "https://gateway.marvel.com/v1/public"
 export const BASE_URL_ZIPCODE_API = "https://ws.apicep.com/cep/"
 export const MARVEL_ENDPOINTS = {
     COMICS:`/comics`,
