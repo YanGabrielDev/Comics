3de80e0309d91718e101f0ab3fbc0f40e1ebb94c
Yan <yangabriel2012@gmail.com>
2023-09-03 16:29:04 -0300

    fix: corrigindo o tratamento de quando nao tem descrição

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,36 +1,41 @@
 import { useFormat } from "../../hooks/useFormat";
-import thanos from '../../assets/svgs/thanos.svg'
+import thanos from "../../assets/svgs/thanos.svg";
 import {
   DetailsContainer,
   DetailsDescription,
   DetailsPriceContainer,
-  DetailsComicTitle
+  DetailsComicTitle,
 } from "./styles";
 import P from "prop-types";
 import { Content } from "../Content";
 export const Details = ({ detailsComics }) => {
   const { prices, title, description, thumbnail } = detailsComics;
-  const {path, extension} = thumbnail
+  const { path, extension } = thumbnail;
   const hqImage = `${path}.${extension}`;
+  const checkDescription = description !== null && description.length !== 0
 
   const { formatCurrency } = useFormat();
   const hqPrice = formatCurrency(prices[0].price);
   return (
- <>
- <DetailsComicTitle>{title}</DetailsComicTitle>
-    <DetailsContainer>
-        <img src={hqImage} loading="lazy"/>
-      <Content>
-        <DetailsDescription>
-          {description ?? "Ops parece que o Thanos passou por aqui..."}
-        </DetailsDescription>
-        {description ?? <img className="thanos-image" src={thanos} loading="lazy" />}
-        <DetailsPriceContainer>
-          <strong>{hqPrice}</strong>
-        </DetailsPriceContainer>
-      </Content>
-    </DetailsContainer>
- </>
+    <>
+      <DetailsComicTitle>{title}</DetailsComicTitle>
+      <DetailsContainer>
+        <img src={hqImage} loading="lazy" />
+        <Content>
+          <DetailsDescription>
+            {checkDescription
+              ? description
+              : "Ops parece que o Thanos passou por aqui..."}
+          </DetailsDescription>
+          {!checkDescription && (
+            <img className="thanos-image" src={thanos} loading="lazy" />
+          )}
+          <DetailsPriceContainer>
+            <strong>{hqPrice}</strong>
+          </DetailsPriceContainer>
+        </Content>
+      </DetailsContainer>
+    </>
   );
 };
 
