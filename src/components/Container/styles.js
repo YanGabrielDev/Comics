5ad9db361d586bd00dcb987954c926473131606b
Yan <yangabriel2012@gmail.com>
2023-09-03 19:39:56 -0300

    style: removendo padding

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -2,7 +2,6 @@ import { styled } from "styled-components";
 
 export const Content = styled.main`
     padding: 0 10%;
-    padding-bottom: 4rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
