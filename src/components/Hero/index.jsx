eb0ad31440854f40239697b7a2dea4f4d4630591
Yan <yangabriel2012@gmail.com>
2023-09-03 16:58:20 -0300

    fix: corrigindo texto

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
     return(
         <HeroContainer>
             <HeroDetails>
-            <HeroTitle>Enconte a sua HQ favorita na melhor loja de Comics ja criada!</HeroTitle>
+            <HeroTitle>Encontre a sua HQ favorita na melhor loja de Comics ja criada!</HeroTitle>
             <HeroSubTitle>Explore o universo das HQs na Loja de Comics!</HeroSubTitle>
             </HeroDetails>
             <HeroImage src={heroImage} alt="logo do deadpool"/>
