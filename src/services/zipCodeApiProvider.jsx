32e9dbf8c40f1362b649f11d00310a788f76b72f
Yan <yangabriel2012@gmail.com>
2023-09-03 21:53:14 -0300

    fix: tratado erro

--- src/services/zipCodeApiProvider.jsx
+++ src/services/zipCodeApiProvider.jsx
@@ -1,12 +1,21 @@
 import { api } from "./api";
 import { BASE_URL_ZIPCODE_API } from "./restApiConstants";
+import { toast } from 'react-toastify';
+
 
 export const zipCodeApiProvider = {
   getAddressByZipCode: async (zipcode) => {
     try {
       const response = await api.get(`${BASE_URL_ZIPCODE_API}${zipcode}.json`);
+      if(response.data.status === 404){
+        toast.error("CEP não encontrado!")
+      }else{
+
+          toast.success("CEP encontrado!")
+      }
       return response.data;
     } catch (error) {
+
       console.error("Erro ao buscar Address:", error);
     }
   },
